93d54bd8e374ed1b0272011a17bc93f7b77ce85c
prjamming <124306301+prjamming@users.noreply.github.com>
2023-09-05 16:18:06 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { createServer as createHttpServer } from "node:http";
 import { readFileSync, existsSync } from "node:fs";
 import serveStatic from "serve-static";
 
-// The following message MAY NOT be removed
-console.log("Hypertabs\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.\n");
-
 const bare = createBareServer("/bare/");
 const serve = serveStatic(fileURLToPath(new URL("../static/", import.meta.url)), { fallthrough: false });
 var server;
@@ -36,13 +33,6 @@ server.on("upgrade", (req, socket, head) => {
 server.on('listening', () => {
   const addr = server.address();
 
-  console.log(`Server running on port ${addr.port}`)
-	console.log('');
-  console.log('You can now view it in your browser.')
-  /* Code for listing IPS from website-aio */
-	console.log(`Local: http://${addr.family === 'IPv6' ? `[${addr.address}]` : addr.address}:${addr.port}`);
-	try { console.log(`On Your Network: http://${address.ip()}:${addr.port}`); } catch (err) {/* Can't find LAN interface */};
-	if(process.env.REPL_SLUG && process.env.REPL_OWNER) console.log(`Replit: https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`);
-});
+
 
 server.listen({ port: (process.env.PORT || 8080) })
